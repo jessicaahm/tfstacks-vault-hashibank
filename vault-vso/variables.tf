e19437ab9a04efc52e856e7b8892eae6ede2c295
jessicaahm <jessica.ang@hashicorp.com>
2025-10-26 23:02:46 +0800

    initial draft

--- vault-vso/variables.tf
+++ vault-vso/variables.tf
@@ -1,3 +1,13 @@
 variable "eks_cluster_name" {
   type    = string
 }
+
+variable "cluster_endpoint" {
+  type        = string
+  description = "EKS cluster endpoint"
+}
+
+variable "cluster_certificate_authority_data" {
+  type        = string
+  description = "EKS cluster certificate authority data"
+}
